6b20dccc9d38021e5f91b522e072952684ae4d15
bcemsume <contact@cemsume.com>
2019-01-31 10:42:05 +0300

    get activetext editor text and replace ts class

--- src/extension.ts
+++ src/extension.ts
@@ -6,22 +6,44 @@ import * as vscode from 'vscode';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "csharptotsclass" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('extension.bcemsume.chsarpToTS', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			return; // No open text editor
+		}
 
+		const selection = editor.selection;
+		let text = editor.document.getText(selection);
+
+
+		editor.edit(builder => builder.replace(selection, replace(text)))
+
+		vscode.window.showInformationMessage(text);
+
+	});
 	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
+
+
+function replace(str: string): string {
+	let result = '';
+	str.split('\n').forEach(element => {
+		if(element.indexOf('class') > -1)
+		{
+			result += element.trim().replace('public class','export class').replace('class','export class');
+		}else if  (element.trim() === '{') {
+			result += element;
+		}else if (element.trim().indexOf('{get;set;}'))
+		{
+			result += element.replace('{get;set;}','');
+		}
+	});
+
+	return result;
+}
